ce8b195fdd434eec4c26ed4e15d5db859a66907d
HarryPaek <paek.harry@gmail.com>
2016-05-20 18:52:40 +0900

    Added HomeCinema.Web Projects

---
+++ HomeCinema/HomeCinema.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace HomeCinema.Web
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            // Configure Autofac
+            AutofacWebapiConfig.Initialize(GlobalConfiguration.Configuration);
+
+            // Configure AutoMapper
+            // AutoMapperConfiguration.Configure();
+        }
+    }
+}
